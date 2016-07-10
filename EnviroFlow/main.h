d68a5a9537ab55a7ccb40ab9f7139b112bc680a6
Akash Bakshi <Akash Bakshi@DESKTOP-2RSMBFD>
2016-07-10 13:32:18 -0400

    Add project files.

---
+++ EnviroFlow/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+//boolean to keep loop going
+bool running;
+
+//MainWindow declaration 
+HWND MainWindow;
+
+HINSTANCE hInst
