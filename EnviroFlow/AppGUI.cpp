df22e4fe46558c178717b3ddef993905f7b7aa03
Akash Bakshi (PC) <akash1996@hotmail.com>
2016-07-11 00:18:44 -0400

    - Changed Background color of Property Window to a light gray

--- EnviroFlow/AppGUI.cpp
+++ EnviroFlow/AppGUI.cpp
@@ -25,9 +25,13 @@ void AppGUI::CreateToolbars()
 {
 	// Code to turn IDD_Properites Dialog as a child windows in Main Window.
 	SecWin[0] = CreateDialog(hInst, MAKEINTRESOURCE(IDD_PROPERTIES), MainWindow, (DLGPROC)PropertiesToolbar);
+	
 	//Set Child Window Position
 	SetWindowPos(SecWin[0], HWND_TOPMOST,1550,50,370,285,SWP_SHOWWINDOW);
 	//Make Window Visible  and set parent window.
 	ShowWindow(SecWin[0], SW_SHOW);
 	SetParent(SecWin[0], MainWindow);
+
+	//Change Background color of Window to a light gray.
+	SetClassLongPtr(SecWin[0], GCLP_HBRBACKGROUND, (LONG)CreateSolidBrush(RGB(72,72,72)));
 }
